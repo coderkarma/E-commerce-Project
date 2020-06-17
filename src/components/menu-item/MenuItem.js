afccc3a95847a3f78f5c9631a809a9ab23f24644
Karma Drukpa <karmadrukpa11@gmail.com>
2020-06-16 17:35:04 -0700

    Styled menu item component

--- src/components/menu-item/MenuItem.js
+++ src/components/menu-item/MenuItem.js
@@ -3,11 +3,13 @@ import './MenuItemStyles.scss';
 
 const MenuItem = ({ title, imageUrl, size }) => {
 	return (
-		<div
-			className={`${size} menu-item`}
-			style={{ background: `url(${imageUrl})` }}>
+		<div className={`${size} menu-item`}>
+			<div
+				className='background-image'
+				style={{ background: `url(${imageUrl})` }}
+			/>
 			<div className='content'>
-				<h1 className='title'>{title}</h1>
+				<h1 className='title'>{title.toUpperCase()}</h1>
 				<span className='subtitle'>SHOP NOW</span>
 			</div>
 		</div>

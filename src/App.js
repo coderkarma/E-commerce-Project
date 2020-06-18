03006437f80df3e1c3275a6714291517092dc945
Karma Drukpa <karmadrukpa11@gmail.com>
2020-06-17 23:56:17 -0700

    Added withRouter

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import HomePage from './pages/homepage/HomePage';
 import './App.css';
 
+
 const App = () => {
 	return (
 		<div>
--- src/components/menu-item/MenuItem.js
+++ src/components/menu-item/MenuItem.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
+
 import './MenuItemStyles.scss';
 
-const MenuItem = ({ title, imageUrl, size }) => {
+const MenuItem = ({ title, imageUrl, size, history }) => {
 	return (
-		<div className={`${size} menu-item`}>
+		<div className={`${size} menu-item`} oncClick={() => history.push()}>
 			<div
 				className='background-image'
 				style={{ background: `url(${imageUrl})` }}
@@ -16,4 +18,4 @@ const MenuItem = ({ title, imageUrl, size }) => {
 	);
 };
 
-export default MenuItem;
+export default withRouter(MenuItem);
